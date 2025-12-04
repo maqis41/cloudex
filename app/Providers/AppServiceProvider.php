226b397ed72b3d827d10ba9ebf80dfe3d82422b7
maqis41 <144024666+maqis41@users.noreply.github.com>
2025-12-04 12:00:54 +0700

    pengubahan http ke https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,17 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Detect HTTPS behind Railway reverse proxy
-        if ($this->app->environment('production')) {
-
-            // Jika request datang via proxy HTTPS (X-Forwarded-Proto), paksa scheme ke https
-            if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
-                $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-                URL::forceScheme('https');
-            }
-
-            // Cara umum: tetap force https
-            URL::forceScheme('https');
-        }
+        //
     }
 }
